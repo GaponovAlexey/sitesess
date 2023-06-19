4cfe7c3f5b3bea3066ef1b718b6b8a9497849a65
Kekelberi <alexei.gaponov@gmail.com>
2023-06-18 22:57:12 -0500

    save

--- components/Burger/page.tsx
+++ components/Burger/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import s from "../../styles/layout.module.scss"
 import ScrollPath from "../Utils/ScrollPath"
 
@@ -10,16 +9,19 @@ const path = [
 ]
 
 function Burger({ setIsOpen }: any) {
-  // const pathname = usePathname()
-
   return (
     <div onClick={() => setIsOpen(false)} className={s.burger_main}>
       <section className={s.burger_wrapper}>
         {path.map((path) => {
           return (
-            <div key={path.id} >
+            <div key={path.id}>
               <ScrollPath to={path.rout}>
-                <div className={s.burger_routs} onClick={() => setIsOpen(false)}>{path.name}</div>
+                <div
+                  className={s.burger_routs}
+                  onClick={() => setIsOpen(false)}
+                >
+                  {path.name}
+                </div>
               </ScrollPath>
             </div>
           )
