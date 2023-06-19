51540060134aec7ce72a06a34763fc13631740c4
Kekelberi <alexei.gaponov@gmail.com>
2023-06-18 20:09:15 -0500

    save

--- components/Utils/ScrollPath.tsx
+++ components/Utils/ScrollPath.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { Link } from "react-scroll"
-
-const ScrollPath = ({ to, children }: any) => (
-  <Link to={to} smooth={true} duration={1500}>
-    {children}
-  </Link>
-)
+// import {  } from 'react-scroll'
+const ScrollPath = ({ to, children }: any) => {
+  return (
+    <Link to={to} smooth={true} duration={1500}>
+      {children}
+    </Link>
+  )
+}
 
 export default ScrollPath
