9f752d41483f774f54b66bcf9f6ea084afd224d1
Kekelberi <alexei.gaponov@gmail.com>
2023-06-30 00:35:35 -0500

    ava

--- components/Pages/Front.tsx
+++ components/Pages/Front.tsx
@@ -3,11 +3,12 @@ import { motion } from "framer-motion"
 import s from "../../styles/main.module.scss"
 import CustomBut from "../Utils/CustomBut"
 import ScrollPath from "../Utils/ScrollPath"
+import Image from "next/image"
 
 const Front = () => {
   return (
     <div className={s.front}>
-      <div id="home">
+      <div id="home" className="mx-auto">
         <motion.h1
           className={s.logoText}
           initial={{ x: +10, opacity: 0 }}
@@ -15,6 +16,20 @@ const Front = () => {
           transition={{ delay: 0.1 }}
         >
           Sitesess.ca
+          <motion.div
+            className={s.logoTextLogo}
+            initial={{ x: +10, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.1 }}
+          >
+            <Image
+              width={100}
+              className="mx-auto"
+              height={100}
+              src="/assets/sitesess4.svg"
+              alt="logo"
+            />
+          </motion.div>
         </motion.h1>
         <motion.div
           className={s.devLogoText}
@@ -25,8 +40,8 @@ const Front = () => {
           The web development company
         </motion.div>
         <motion.h2
-         initial={{ x: +10, opacity: 0 }}
-         animate={{ x: 0, opacity: 1 }}
+          initial={{ x: +10, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
           transition={{ delay: 0.6 }}
         >
           We Know You Are Passionate About Your Small Business
