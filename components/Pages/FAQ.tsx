5008efba8d7c8ff5617c63a6804f0a01add2b51d
Kekelberi <alexei.gaponov@gmail.com>
2023-06-30 16:43:45 -0500

    save

--- components/Pages/FAQ.tsx
+++ components/Pages/FAQ.tsx
@@ -5,67 +5,53 @@ import s from "../../styles/main.module.scss"
 const DataAccordion = [
   {
     id: 1,
-    title: "FAQ:",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "What is included in the cost of web development?",
+    body: `Answer: The cost of web development includes the design and development of the website, testing, search engine optimization (SEO), and basic support after launch. Additional services such as advanced SEO, content management, and social media promotion are typically billed separately.`,
   },
   {
     id: 2,
-    title: "Product Descriptions: ",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "How long will it take to develop my website?",
+    body: `Answer: The timeline for website development can vary depending on the complexity of the project. Typically, developing a basic website for a small business takes between 4 to 6 weeks, including time for project discussion, design, development, testing, and launch.`,
   },
   {
     id: 3,
-    title: "Workflow:",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "Can I update my website myself after it's launched?",
+    body: `Answer: Yes, we provide a content management system that allows you to easily add, edit, and delete content on your website. We will also provide training on how to use this system.`,
   },
   {
     id: 4,
-    title: "Terms and Conditions: ",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "What type of support do you provide after the website launch?",
+    body: `Answer: We provide basic technical support for 30 days after the launch of your website at no additional cost. This includes fixing any bugs that may arise and assistance in using the content management system. Additional support and maintenance are available for an extra fee.`,
   },
   {
     id: 5,
-    title: "Accordion 5",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "Do you have a portfolio of work that I can view?",
+    body: `Answer: Yes, you can view our previous work in the "Portfolio" section of our website. We proudly showcase the variety of projects that we have executed for our clients.`,
   },
   {
     id: 6,
-    title: "Accordion 6",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "Do I need to provide the content for my website?",
+    body: `Answer: While we highly encourage clients to provide their own content as it adds authenticity, we also have content creation services available if needed. This includes writing, editing, and sourcing images or videos.`,
   },
   {
     id: 7,
-    title: "Accordion 7",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "Can you help with improving the visibility of my website on search engines?",
+    body: `Answer: Yes, we provide search engine optimization (SEO) services to improve your website's visibility on search engines like Google. This includes keyword research, on-page optimization, link building, and more.`,
   },
   {
     id: 8,
-    title: "Accordion 8",
-    body: `<em>text.</em> It is hidden by default, until the collapse
-  plugin adds the appropriate classes that we use to style each
-  element. These classes control the overall appearance, as well
-  as the showing and hiding via CSS transitions.`,
+    title: "Do you provide web hosting services?",
+    body: `Answer: While we primarily focus on web design and development, we do offer hosting services as an additional option. We can also work with your preferred hosting provider if you already have one.`,
+  },
+  {
+    id: 9,
+    title: "What is a responsive design and will my website be responsive?",
+    body: `Answer: Responsive design ensures your website adapts to the screen size and device it is being viewed on, providing a great user experience whether on a desktop, tablet, or smartphone. Yes, all our websites are designed to be responsive.`,
+  },
+  {
+    id: 10,
+    title: "What happens if my website encounters an issue or goes down?",
+    body: `Answer: We provide ongoing support packages that include website monitoring and immediate response to any issues. While website downtimes are rare, we understand the importance of a quick response and will prioritize resolving any issues that arise.`,
   },
 ]
 
@@ -74,9 +60,7 @@ const FAQ = () => {
 
   return (
     <div className="grid px-[2%] md:grid-cols-2">
-      <div
-        className='mt-[10%] mb-[20%] lg:mb-20'
-      >
+      <div className="container mt-[10%] mb-[20%] lg:mb-20">
         {DataAccordion?.map((accord) => {
           return (
             <div
@@ -112,7 +96,7 @@ const FAQ = () => {
                 >
                   <div>
                     <p
-                      className="text-gray-800 max-w-[30rem] dark:text-white"
+                      className="text-gray-800 max-w-[480px] dark:text-white"
                       dangerouslySetInnerHTML={{ __html: accord.body }}
                     ></p>
                   </div>
